47880af3b5e2e06555756b731a7b71a691b62a90
moulin <yoshimura-takuya@kayac.com>
2017-02-15 20:43:51 +0900

    `to` is not neccesary if `registration_ids` exists.

--- gcm/request.go
+++ gcm/request.go
@@ -2,7 +2,7 @@ package gcm
 
 // Payload for gcm
 type Payload struct {
-	To                    string        `json:"to"`
+	To                    string        `json:"to,omitempty"`
 	RegistrationIDs       []string      `json:"registration_ids,omitempty"`
 	CollapseKey           string        `json:"collapse_key,omitempty"`
 	Priority              string        `json:"priority,omitempty"`
