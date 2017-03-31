e907e1a68bad23690e6c5deda90813a09aeb22a9
FUJIWARA Shunichiro <fujiwara.shunichiro@gmail.com>
2017-03-31 18:47:46 +0900

    change interfaces of Client.Send()
    Send() returns []Result, not a Response(=interface{}).

--- fcm/client.go
+++ fcm/client.go
@@ -23,59 +23,58 @@ type Client struct {
 }
 
 // Send sends notifications to fcm (TODO: send retry)
-func (gc *Client) Send(p Payload) (*Response, error) {
-	req, err := gc.NewRequest(p)
+func (c *Client) Send(p Payload) ([]Result, error) {
+	req, err := c.NewRequest(p)
 	if err != nil {
 		return nil, err
 	}
 
-	res, err := gc.Client.Do(req)
+	res, err := c.Client.Do(req)
 	if err != nil {
 		return nil, err
 	}
 	defer res.Body.Close()
 
-	resp := &Response{}
+	var body ResponseBody
 	dec := json.NewDecoder(res.Body)
-	if err = dec.Decode(&resp.Body); err != nil {
-		return nil, err
-	}
-	resp.Header.StatusCode = res.StatusCode
-
-	if res.StatusCode == http.StatusOK {
-		return resp, err
+	if err = dec.Decode(&body); err != nil {
+		return nil, NewError(res.StatusCode, err.Error())
 	}
 
-	var errCodes []string
-	if resp.Body.Error != "" {
-		errCodes = append(errCodes, resp.Body.Error)
-	} else {
-		for _, msg := range resp.Body.Results {
-			if msg.Error != "" {
-				errCodes = append(errCodes, msg.Error)
-			}
+	if res.StatusCode != http.StatusOK {
+		return nil, NewError(res.StatusCode, "status is not OK")
+	}
+
+	if len(p.RegistrationIDs) == 0 && len(body.Results) == 1 {
+		r := body.Results[0]
+		r.To = p.To
+		r.StatusCode = res.StatusCode
+		return []Result{r}, nil
+	} else if len(p.RegistrationIDs) == len(body.Results) {
+		ret := make([]Result, 0, len(body.Results))
+		for i, r := range body.Results {
+			r.RegistrationID = p.RegistrationIDs[i]
+			r.StatusCode = res.StatusCode
+			ret = append(ret, r)
 		}
+		return ret, nil
 	}
-	eres := ErrorResponse{
-		StatusCode: resp.Header.StatusCode,
-		ErrCodes:   errCodes,
-	}
-	return nil, eres
+
+	return nil, NewError(res.StatusCode, "unexpected response")
 }
 
 // NewRequest creates request for fcm
-func (gc *Client) NewRequest(p Payload) (*http.Request, error) {
-
+func (c *Client) NewRequest(p Payload) (*http.Request, error) {
 	data, err := json.Marshal(p)
 	if err != nil {
 		return nil, err
 	}
 
-	req, err := http.NewRequest("POST", gc.endpoint.String(), bytes.NewReader(data))
+	req, err := http.NewRequest("POST", c.endpoint.String(), bytes.NewReader(data))
 	if err != nil {
 		return nil, err
 	}
-	req.Header.Set("Authorization", fmt.Sprintf("key=%s", gc.apiKey))
+	req.Header.Set("Authorization", fmt.Sprintf("key=%s", c.apiKey))
 	req.Header.Set("Content-Type", "application/json")
 
 	return req, nil
@@ -87,17 +86,17 @@ func NewClient(apikey string, endpoint *url.URL, timeout time.Duration) *Client 
 		Timeout: timeout,
 	}
 
-	gc := &Client{
+	c := &Client{
 		apiKey: apikey,
 		Client: client,
 	}
 
 	if endpoint != nil {
-		gc.endpoint = endpoint
+		c.endpoint = endpoint
 
 	} else {
-		gc.endpoint, _ = url.Parse(DefaultFCMEndpoint)
+		c.endpoint, _ = url.Parse(DefaultFCMEndpoint)
 	}
 
-	return gc
+	return c
 }
